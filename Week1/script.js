d0aa85e8a6fc954e06475a2d91c1ef62c13b3ce3
Anneice Manzanares <mountains.stars@gmail.com>
2023-01-27 12:14:07 -0700

    Added color change mouse over event

--- Week1/script.js
+++ Week1/script.js
@@ -1,19 +1,8 @@
-let recipeList=document.querySelector('ol li')
+const colorChange = document.getElementById('ingredients');
 
-let recipeStep=1;
-for (let step of recipeList) {
-    let span = document.createElement('span');
-    let checkbox=document.createElement('input');
-    checkbox.className='checked';
-    span.id=`step -${recipeStep}`;
-    checkbox.type='checkbox';
-    checkbox.name='step';
-    checkbox.value=step.textContent;
-
-    span.textContent= step.textContent;
-
-    step.textContent="";
-    step.appendChild(span);
-    span.prepend(checkbox)
-    recipeStep += 1;
-}
+colorChange.addEventListener('mouseover', (event)=> {
+    event.target.style.color = "cyan";
+    setTimeout(()=> {
+        event.target.style.color="";
+    }, 500)
+}, false)
