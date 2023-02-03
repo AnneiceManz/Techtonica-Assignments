4c56194bd6ef80149cee779b7f1ed87365331779
Anneice Manzanares <mountains.stars@gmail.com>
2023-02-03 15:32:49 -0700

    added event listener to form button

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -11,3 +11,17 @@ function changeClass() {
       btn.innerHTML = "Show More";
   }
 }
+
+let name= document.getElementById('name');
+let email=document.getElementById('email');
+let phone=document.getElementById('phone');
+let message=document.getElementById('message');
+
+document.getElementById('form').addEventListener('submit', function(event){
+    event.preventDefault();
+    let alert= "";
+    let messageArea=document.getElementById('messageArea');
+    alert="Thankn you for your message!"
+    messageArea.innerHTML=alert
+    console.log(name.value,email.value,phone.value,message.value)
+})
