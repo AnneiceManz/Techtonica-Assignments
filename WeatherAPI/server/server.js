d2b7f811a22d1c0ce1c9a4ccb2a2ca781bd6bd03
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-02 14:33:09 -0700

    update to api call

--- WeatherAPI/server/server.js
+++ WeatherAPI/server/server.js
@@ -16,7 +16,23 @@ app.get('/', (req, res) => {
 
 // creates an endpoint for the route /api/weather
 app.get('/api/weather', (req, res) => {
-  res.json(dataWeather);
+  const city =req.query.cityName;
+  const apiKey = process.env.API_KEY;
+  const params = new URLSearchParams ({
+    q: req.query.cityName,
+    appid: process.env.API_KEY,
+    units: "imperial",
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({data});
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
 // console.log that your server is up and running
