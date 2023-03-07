52605c1ab163f84cb4dea5f1356714af05380468
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-07 16:09:22 -0700

    created err test

--- week8project/server/index.js
+++ week8project/server/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import fetch from 'node-fetch';
 
 // For testing my API I saving one response in a .js file 
-import fakedata  from './fakedata.js';
+// import fakedata  from './fakedata.js';
 
 const app = express();
 const PORT = 5050;
@@ -18,15 +18,23 @@ app.use(bodyParser.json());
 
 // //creates an endpoint for the route `/`
 app.get("/", (req, res) => {
-    res.json("Hello Anneice.This is your Server for your game.");
+    res.json("Hello Anneice. This is your Server for your game.");
   });
 
 // Make the GET request for the GAME Api for grabbing all the questions 
 
 
   // //hardcode the game response for testing reasons to don't saturate my API call. 
-app.get('/api/game', (req, res) =>{
-    res.json(fakedata);
+app.get('/api/game', async (req, res) =>{
+  try {
+    const URL ="https://opentdb.com/api.php?amount=15&category=32&difficulty=easy&type=multiple"
+    const apiRequest = await fetch(URL);
+    const questions = await apiRequest.json();
+    res.send({questions})
+  } catch (err) {
+    console.log(err)
+  }
+    // res.json(fakedata);
 })
 
 
