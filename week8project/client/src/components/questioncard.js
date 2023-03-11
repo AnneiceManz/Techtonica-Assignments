83b22d877e5a39a4e70e3fd0afb56599af6e281e
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-10 17:45:28 -0700

    updated questionCard syntax

--- week8project/client/src/components/questioncard.js
+++ week8project/client/src/components/questioncard.js
@@ -1,43 +1,44 @@
-import { decode } from 'html-entities'
+import { useState } from 'react';
 
 const QuestionCard = (props) => {
-
-  //Decode questions
-  const question = decode(props.question.question);
-
-  //Create new array of choices from correct and incorrect answers
-  let answers = [props.question.correct_answer, ...props.question.incorrect_answers];
-
-  //decode answer choices
-  answers = answers.map((answer) => decode(answer))
-
-  //create decoded correct answer variable
-  const correct_answer = answers[0]
-
-  //shuffle answers array
-  const shuffledAnswers = answers.sort((a,b) => 0.5 - Math.random());
-
-  const handleUserAnswer = (e) => {
-    if (shuffledAnswers[e.target.value] === correct_answer) {
-      props.setScore(props.score + 1);
-      props.setQuestionNumber(props.questionNum +1);
-      e.target.checked = false
-    }
-  }
+    const [disable, setDisable] = useState(false)
+    console.log(props.questionsAnswers, "inside question.js")
+    
+    const handleAnswerClick = (e) => {
+        
+    const nextQuestion = props.currentQuestion + 1;
+    if (nextQuestion < props.questions.length) {
+                props.setCurrentQuestion(nextQuestion);
+            } else {
+                setDisable(true);
+                // alert('This is the end of the quiz!');
+            }   
+    if (props.questionsAnswers[e.target.value].isCorrect === true) {
+            props.setScore+1
+      }
+}
     
     return (
-      <div className={"question-section"}>
-        <h3>{question}</h3>
-        <div className="answers">
-          <form>
-            <button onChange={handleUserAnswer}>{shuffledAnswers[0]}</button>
-            <button onChange={handleUserAnswer}>{shuffledAnswers[1]}</button>
-            <button onChange={handleUserAnswer}>{shuffledAnswers[2]}</button>
-            <button onChange={handleUserAnswer}>{shuffledAnswers[3]}</button>
-          </form>
-        </div>
-      </div>
-    );
-  };
+    <div>{disable ? <div className="score-section">You score: {props.score}/15</div> :
+        <div className="question-section">
+            <div className="question-text">
+                <h1>{props.questionsAnswers[4]}</h1>
+            </div>
+            <div className='answer-section'>   
+            {// eslint-disable-next-line
+                props.questionsAnswers.map((currArray, index) => {
+                    if(index!==4) {
+                return  <button 
+           onClick={handleAnswerClick}
+           key={index} type="button" value={index}>
+            {currArray.answerText}
+            </button>
+                }
+            })}
+        </div> 
+    </div>}
+</div>
+    )
+}
 
 export default QuestionCard;
